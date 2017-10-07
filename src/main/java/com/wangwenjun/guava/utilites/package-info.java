5b451a346efd2ffebd18791ee6410ea5eac8170b
wangwenjun <532500648@qq.com>
2017-10-07 15:30:48 +0800

    add the utils

---
+++ src/main/java/com/wangwenjun/guava/utilites/package-info.java
@@ -0,0 +1,9 @@
+/***************************************
+ * @author:Alex Wang
+ * @Date:2017/10/6
+ * @QQ: 532500648
+ *
+ * The utilites of Joiner will be demo by test case {@link JoinerTest}
+ * The utilites of Splitter will be demo by test case {@link SplitterTest}
+ ***************************************/
+package com.wangwenjun.guava.utilites;
