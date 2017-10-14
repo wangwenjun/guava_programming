c6e797b32e13769c1379e3d55d823b1b972538b2
wangwenjun <532500648@qq.com>
2017-10-14 22:08:42 +0800

    add the functional programming for guava

---
+++ src/test/java/com/wangwenjun/guava/io/CharStreamsTest.java
@@ -0,0 +1,16 @@
+package com.wangwenjun.guava.io;
+
+import org.junit.Test;
+
+/***************************************
+ * @author:Alex Wang
+ * @Date:2017/10/14
+ * @QQ: 532500648
+ ***************************************/
+public class CharStreamsTest {
+
+    @Test
+    public void testCharStreams() {
+
+    }
+}
