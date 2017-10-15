bec560bcef7e9592f9d260b2cfbf92cef936a3c4
wangwenjun <532500648@qq.com>
2017-10-15 15:39:20 +0800

    add the functional programming for guava

--- src/main/java/com/wangwenjun/guava/io/Base64.java
+++ src/main/java/com/wangwenjun/guava/io/Base64.java
@@ -58,5 +58,50 @@ public final class Base64 {
         return binaryResult.toString();
     }
 
+    public static String decode(final String encrypt) {
+        StringBuilder resultBuilder = new StringBuilder();
 
+        String temp = encrypt;
+        int equalIndex = temp.indexOf("=");
+        if (equalIndex > 0) {
+            temp = temp.substring(0, equalIndex);
+        }
+
+        String base64Binary = toBase64Binary(temp);
+        for (int i = 0; i < base64Binary.length() / 8; i++) {
+            int begin = i * 8;
+            String str = base64Binary.substring(begin, begin + 8);
+            char c = Character.toChars(Integer.valueOf(str, 2))[0];
+            resultBuilder.append(c);
+        }
+
+        return resultBuilder.toString();
+    }
+
+    private static String toBase64Binary(final String source) {
+        final StringBuilder binaryResult = new StringBuilder();
+        for (int index = 0; index < source.length(); index++) {
+            int i = CODE_STRING.indexOf(source.charAt(index));
+            String charBin = Integer.toBinaryString(i);
+            int delta = 6 - charBin.length();
+            for (int d = 0; d < delta; d++) {
+                charBin = "0" + charBin;
+            }
+
+            binaryResult.append(charBin);
+        }
+
+        return binaryResult.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(encode("hello"));
+        System.out.println(encode("a"));
+        System.out.println(encode("12a"));
+        System.out.println("=======================");
+        System.out.println(decode("aGVsbG8="));
+        System.out.println(decode("YQ=="));
+        System.out.println(decode("MTJh"));
+    }
 }
