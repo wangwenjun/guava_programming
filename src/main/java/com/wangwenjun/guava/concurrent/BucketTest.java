14835a17bdc0a81fa602178654d678a1eecaa599
wangwenjun <532500648@qq.com>
2017-11-12 14:49:44 +0800

    add the rateLimiter demo

---
+++ src/main/java/com/wangwenjun/guava/concurrent/BucketTest.java
@@ -0,0 +1,58 @@
+package com.wangwenjun.guava.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static java.lang.Thread.currentThread;
+
+/***************************************
+ * @author:Alex Wang
+ * @Date:2017/11/8
+ ***************************************/
+public class BucketTest
+{
+
+    public static void main(String[] args)
+    {
+        final Bucket bucket = new Bucket();
+        final AtomicInteger DATA_CREATOR = new AtomicInteger(0);
+
+        IntStream.range(0, 5).forEach(i ->
+        {
+            new Thread(() ->
+            {
+                for (; ; )
+                {
+                    int data = DATA_CREATOR.getAndIncrement();
+                    bucket.submit(data);
+                    try
+                    {
+                        TimeUnit.MILLISECONDS.sleep(200L);
+                    } catch (Exception e)
+                    {
+                        if (e instanceof IllegalStateException)
+                        {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }       //25
+
+                //10
+
+                //5:2
+            }).start();
+        });
+
+
+        IntStream.range(0, 5)
+                .forEach(i -> new Thread(() ->
+                        {
+                            for (; ; )
+                            {
+                                bucket.takeThenConsume(x -> System.out.println(currentThread() + " W " + x));
+                            }
+                        }).start()
+                );
+    }
+}
