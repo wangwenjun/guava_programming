4f2fa395ac62d56b4d2cc3154f5f0edc58b116ab
wangwenjun <532500648@qq.com>
2017-11-18 21:19:29 +0800

    add the cache demo

---
+++ src/main/java/com/wangwenjun/guava/cache/Employee.java
@@ -0,0 +1,46 @@
+package com.wangwenjun.guava.cache;
+
+import com.google.common.base.MoreObjects;
+
+/***************************************
+ * @author:Alex Wang
+ * @Date:2017/11/18
+ * QQ: 532500648
+ * QQ群:463962286
+ ***************************************/
+public class Employee
+{
+    private final String name;
+    private final String dept;
+    private final String empID;
+
+    public Employee(String name, String dept, String empID)
+    {
+        this.name = name;
+        this.dept = dept;
+        this.empID = empID;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getDept()
+    {
+        return dept;
+    }
+
+    public String getEmpID()
+    {
+        return empID;
+    }
+
+    @Override
+    public String toString()
+    {
+        return MoreObjects.toStringHelper(this)
+                .add("Name", this.getName()).add("Department", getDept())
+                .add("EmployeeID", this.getEmpID()).toString();
+    }
+}
